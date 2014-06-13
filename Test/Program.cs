2b14aae8fe3ebba509d1e8342bb39e230cb33a80
Alexandre Point <apoint@fleex.tv>
2014-06-13 11:24:58 +0200

    Added Test project to convert binary files to readable files
    Among other things!

---
+++ Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpEntropy;
+using SharpEntropy.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read file
+            var currentDirectory = Environment.CurrentDirectory + "/../../";
+            var binFilePath = currentDirectory + "../Resources/Models/EnglishSD.nbin";
+            var reader = new BinaryGisModelReader(binFilePath);
+            var model = new GisModel(reader);
+
+            var outputFilePath = currentDirectory + "/Output/EnglishSDClear.txt";
+            var writer = new PlainTextGisModelWriter();
+            writer.Persist(model, outputFilePath);
+
+            Console.WriteLine("OK");
+            Console.ReadKey();
+        }
+    }
+}

813d5b7cddc94d7c8fcaa0c05a49cce3992ebad8
Alexandre Point <apoint@fleex.tv>
2015-02-04 17:48:34 +0100

    Updated EnglishTok model after training with latest training files

--- ToolsExample/MainForm.cs
+++ ToolsExample/MainForm.cs
@@ -75,129 +75,129 @@ namespace ToolsExample
 		/// </summary>
 		private void InitializeComponent()
 		{
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this._btnParse = new System.Windows.Forms.Button();
-            this._btnPosTag = new System.Windows.Forms.Button();
-            this._btnChunk = new System.Windows.Forms.Button();
-            this._btnTokenize = new System.Windows.Forms.Button();
-            this._btnNameFind = new System.Windows.Forms.Button();
-            this._txtOut = new System.Windows.Forms.TextBox();
-            this._btnSplit = new System.Windows.Forms.Button();
-            this._txtIn = new System.Windows.Forms.TextBox();
-            this._btnGender = new System.Windows.Forms.Button();
-            this._btnSimilarity = new System.Windows.Forms.Button();
-            this._btnCoreference = new System.Windows.Forms.Button();
+            var resources = new ComponentResourceManager(typeof(MainForm));
+            this._btnParse = new Button();
+            this._btnPosTag = new Button();
+            this._btnChunk = new Button();
+            this._btnTokenize = new Button();
+            this._btnNameFind = new Button();
+            this._txtOut = new TextBox();
+            this._btnSplit = new Button();
+            this._txtIn = new TextBox();
+            this._btnGender = new Button();
+            this._btnSimilarity = new Button();
+            this._btnCoreference = new Button();
             this.SuspendLayout();
             // 
             // btnParse
             // 
-            this._btnParse.Location = new System.Drawing.Point(360, 104);
+            this._btnParse.Location = new Point(360, 104);
             this._btnParse.Name = "_btnParse";
-            this._btnParse.Size = new System.Drawing.Size(75, 23);
+            this._btnParse.Size = new Size(75, 23);
             this._btnParse.TabIndex = 21;
             this._btnParse.Text = "Parse";
-            this._btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            this._btnParse.Click += this.btnParse_Click;
             // 
             // btnPOSTag
             // 
-            this._btnPosTag.Location = new System.Drawing.Point(184, 104);
+            this._btnPosTag.Location = new Point(184, 104);
             this._btnPosTag.Name = "_btnPosTag";
-            this._btnPosTag.Size = new System.Drawing.Size(75, 23);
+            this._btnPosTag.Size = new Size(75, 23);
             this._btnPosTag.TabIndex = 20;
             this._btnPosTag.Text = "POS tag";
-            this._btnPosTag.Click += new System.EventHandler(this.btnPOSTag_Click);
+            this._btnPosTag.Click += this.btnPOSTag_Click;
             // 
             // btnChunk
             // 
-            this._btnChunk.Location = new System.Drawing.Point(272, 104);
+            this._btnChunk.Location = new Point(272, 104);
             this._btnChunk.Name = "_btnChunk";
-            this._btnChunk.Size = new System.Drawing.Size(75, 23);
+            this._btnChunk.Size = new Size(75, 23);
             this._btnChunk.TabIndex = 19;
             this._btnChunk.Text = "Chunk";
-            this._btnChunk.Click += new System.EventHandler(this.btnChunk_Click);
+            this._btnChunk.Click += this.btnChunk_Click;
             // 
             // btnTokenize
             // 
-            this._btnTokenize.Location = new System.Drawing.Point(96, 104);
+            this._btnTokenize.Location = new Point(96, 104);
             this._btnTokenize.Name = "_btnTokenize";
-            this._btnTokenize.Size = new System.Drawing.Size(75, 23);
+            this._btnTokenize.Size = new Size(75, 23);
             this._btnTokenize.TabIndex = 18;
             this._btnTokenize.Text = "Tokenize";
-            this._btnTokenize.Click += new System.EventHandler(this.btnTokenize_Click);
+            this._btnTokenize.Click += this.btnTokenize_Click;
             // 
             // btnNameFind
             // 
-            this._btnNameFind.Location = new System.Drawing.Point(448, 104);
+            this._btnNameFind.Location = new Point(448, 104);
             this._btnNameFind.Name = "_btnNameFind";
-            this._btnNameFind.Size = new System.Drawing.Size(75, 23);
+            this._btnNameFind.Size = new Size(75, 23);
             this._btnNameFind.TabIndex = 16;
             this._btnNameFind.Text = "Find Names";
-            this._btnNameFind.Click += new System.EventHandler(this.btnNameFind_Click);
+            this._btnNameFind.Click += this.btnNameFind_Click;
             // 
             // txtOut
             // 
-            this._txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this._txtOut.Location = new System.Drawing.Point(8, 136);
+            this._txtOut.Anchor = ((AnchorStyles.Top | AnchorStyles.Bottom)
+                                   | AnchorStyles.Left)
+                                  | AnchorStyles.Right;
+            this._txtOut.Location = new Point(8, 136);
             this._txtOut.Multiline = true;
             this._txtOut.Name = "_txtOut";
-            this._txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this._txtOut.Size = new System.Drawing.Size(784, 400);
+            this._txtOut.ScrollBars = ScrollBars.Both;
+            this._txtOut.Size = new Size(784, 400);
             this._txtOut.TabIndex = 15;
             this._txtOut.WordWrap = false;
             // 
             // btnSplit
             // 
-            this._btnSplit.Location = new System.Drawing.Point(8, 104);
+            this._btnSplit.Location = new Point(8, 104);
             this._btnSplit.Name = "_btnSplit";
-            this._btnSplit.Size = new System.Drawing.Size(75, 23);
+            this._btnSplit.Size = new Size(75, 23);
             this._btnSplit.TabIndex = 14;
             this._btnSplit.Text = "Split";
-            this._btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            this._btnSplit.Click += this.btnSplit_Click;
             // 
             // txtIn
             // 
-            this._txtIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this._txtIn.Location = new System.Drawing.Point(8, 8);
+            this._txtIn.Anchor = (AnchorStyles.Top | AnchorStyles.Left)
+                                 | AnchorStyles.Right;
+            this._txtIn.Location = new Point(8, 8);
             this._txtIn.Multiline = true;
             this._txtIn.Name = "_txtIn";
-            this._txtIn.Size = new System.Drawing.Size(784, 88);
+            this._txtIn.Size = new Size(784, 88);
             this._txtIn.TabIndex = 13;
             this._txtIn.Text = resources.GetString("txtIn.Text");
             // 
             // btnGender
             // 
-            this._btnGender.Location = new System.Drawing.Point(539, 104);
+            this._btnGender.Location = new Point(539, 104);
             this._btnGender.Name = "_btnGender";
-            this._btnGender.Size = new System.Drawing.Size(75, 23);
+            this._btnGender.Size = new Size(75, 23);
             this._btnGender.TabIndex = 22;
             this._btnGender.Text = "Gender";
-            this._btnGender.Click += new System.EventHandler(this.btnGender_Click);
+            this._btnGender.Click += this.btnGender_Click;
             // 
             // btnSimilarity
             // 
-            this._btnSimilarity.Location = new System.Drawing.Point(627, 104);
+            this._btnSimilarity.Location = new Point(627, 104);
             this._btnSimilarity.Name = "_btnSimilarity";
-            this._btnSimilarity.Size = new System.Drawing.Size(75, 23);
+            this._btnSimilarity.Size = new Size(75, 23);
             this._btnSimilarity.TabIndex = 23;
             this._btnSimilarity.Text = "Similarity";
-            this._btnSimilarity.Click += new System.EventHandler(this.btnSimilarity_Click);
+            this._btnSimilarity.Click += this.btnSimilarity_Click;
             // 
             // btnCoreference
             // 
-            this._btnCoreference.Location = new System.Drawing.Point(717, 104);
+            this._btnCoreference.Location = new Point(717, 104);
             this._btnCoreference.Name = "_btnCoreference";
-            this._btnCoreference.Size = new System.Drawing.Size(75, 23);
+            this._btnCoreference.Size = new Size(75, 23);
             this._btnCoreference.TabIndex = 24;
             this._btnCoreference.Text = "Coreference";
-            this._btnCoreference.Click += new System.EventHandler(this.btnCoreference_Click);
+            this._btnCoreference.Click += this.btnCoreference_Click;
             // 
             // MainForm
             // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(800, 542);
+            this.AutoScaleBaseSize = new Size(5, 13);
+            this.ClientSize = new Size(800, 542);
             this.Controls.Add(this._btnCoreference);
             this.Controls.Add(this._btnSimilarity);
             this.Controls.Add(this._btnGender);

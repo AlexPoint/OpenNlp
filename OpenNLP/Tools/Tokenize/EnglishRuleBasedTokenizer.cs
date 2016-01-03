7431e7b87e1d5b7f9a948a58c246495f94f843e4
Alex <pointalexandre@gmail.com>
2016-01-03 21:36:28 +0100

    New: optimized performance of EnglishRuleBasedTokenizer

--- OpenNLP/Tools/Tokenize/EnglishRuleBasedTokenizer.cs
+++ OpenNLP/Tools/Tokenize/EnglishRuleBasedTokenizer.cs
@@ -14,13 +14,23 @@ namespace OpenNLP.Tools.Tokenize
     /// </summary>
     public class EnglishRuleBasedTokenizer : AbstractTokenizer
     {
+        private static readonly Regex WhitespaceRegex = new Regex("\\s+", RegexOptions.Compiled);
 
         public override Span[] TokenizePositions(string input)
         {
             if (string.IsNullOrEmpty(input)) { return new Span[0]; }
 
             // split on spaces
-            var parts = SplitOnWhitespaces(input);
+            var parts = new List<Span>();
+            var currentStartOfSpan = 0;
+            var matches = WhitespaceRegex.Matches(input);
+            for(var i = 0; i < matches.Count; i++)
+            {
+                var match = matches[i];
+                parts.Add(new Span(currentStartOfSpan, match.Index));
+                currentStartOfSpan = match.Index + match.Length;
+            }
+            parts.Add(new Span(currentStartOfSpan, input.Length));
             // tokenize each token (if necessary)
             var tokenSpans = parts
                 .SelectMany(sp => SplitToken(input, sp))
@@ -34,7 +44,6 @@ namespace OpenNLP.Tools.Tokenize
                 if (input.Substring(lastSpanWithWordChar.Start, lastSpanWithWordChar.Length()).EndsWith("."))
                 {
                     tokenSpans.RemoveAt(indexOfLastTokenWithWordChar);
-                    //var lastTokenParts = Regex.Split(lastSpanWithWordChar, "(?=\\.$)");
                     var lastSpans = new List<Span>()
                     {
                         new Span(lastSpanWithWordChar.Start, lastSpanWithWordChar.End - 1),
@@ -47,66 +56,64 @@ namespace OpenNLP.Tools.Tokenize
             return tokenSpans.ToArray();
         }
 
-
-        // Tokenize rules 
-        private static readonly List<Regex> TokenizationRegexes = new List<Regex>()
+        private static readonly List<string> TokenizationRules = new List<string>()
         {
             // split before .{2,} if not preceded by '.'
-            new Regex("(?<!\\.)(?=\\.{2,})", RegexOptions.Compiled),
+            "(?<!\\.)(?=\\.{2,})",
             // split after .{2,} if not followed by '.'
-            new Regex("(?<=\\.{2,})(?!\\.)", RegexOptions.Compiled),
+            "(?<=\\.{2,})(?!\\.)",
             
             // split before !+ if not preceded by '!'
-            new Regex("(?<!!)(?=!+)", RegexOptions.Compiled),
+            "(?<!!)(?=!+)",
             // split after !+ if not followed by '!'
-            new Regex("(?<=!+)(?!!)", RegexOptions.Compiled),
+            "(?<=!+)(?!!)",
 
             // split before ?+ if not preceded by '?'
-            new Regex("(?<!\\?)(?=\\?+)", RegexOptions.Compiled),
+            "(?<!\\?)(?=\\?+)",
             // split after ?+ if not followed by '?'
-            new Regex("(?<=\\?+)(?!\\?)", RegexOptions.Compiled),
+            "(?<=\\?+)(?!\\?)",
             
             // split after ',' if not followed directly by figure
-            new Regex("(?<=,)(?!\\d)", RegexOptions.Compiled), 
+            "(?<=,)(?!\\d)",
             // split before ',' if not followed directly by figure
-            new Regex("((?=,\\D)|(?=,$))", RegexOptions.Compiled),
+            "((?=,\\D)|(?=,$))",
             
             // split after ':' if not followed directly by figure
-            new Regex("(?<=:)(?!\\d)", RegexOptions.Compiled), 
+            "(?<=:)(?!\\d)",
             // split before ':' if not followed directly by figure
-            new Regex("((?=:\\D)|(?=:$))", RegexOptions.Compiled),
+            "((?=:\\D)|(?=:$))",
 
             // split before ' at the end of a token
-            new Regex("(?=\\'$)", RegexOptions.Compiled),
-            new Regex("(?=’$)", RegexOptions.Compiled),
+            "(?=\\'$)",
+            "(?=’$)",
 
             // split before - when at the end of a token and not preceded by -
-            new Regex("(?<!\\-)(?=\\-$)", RegexOptions.Compiled),
+            "(?<!\\-)(?=\\-$)",
             // split after - when at the beginning of a token and not followed by -
-            new Regex("(?<=^\\-)(?!\\-)", RegexOptions.Compiled),
+            "(?<=^\\-)(?!\\-)",
 
             // The two following rules are used to split compound words. Ex: x-ray --> x|-|ray
             // Not sure this is the right way to go ultimately so remove them if necessary
             // split after - when followed by a letter
-            new Regex("(?<=\\-)(?=\\w)", RegexOptions.Compiled),
+            "(?<=\\-)(?=\\w)",
             // split before - when preceded by a letter
-            new Regex("(?<=\\w)(?=\\-)", RegexOptions.Compiled),
+            "(?<=\\w)(?=\\-)",
             
             // split before ;, (, ), [, ], {, }, " in all cases
-            new Regex("(?=;|\\(|\\)|\\{|\\}|\\[|\\]|\"|…)", RegexOptions.Compiled),
+            "(?=;|\\(|\\)|\\{|\\}|\\[|\\]|\"|…)",
             // split after ;, (, ), [, ], {, }, " in all cases
-            new Regex("(?<=;|\\(|\\)|\\{|\\}|\\[|\\]|\"|…)", RegexOptions.Compiled),
+            "(?<=;|\\(|\\)|\\{|\\}|\\[|\\]|\"|…)",
             
             // split after ' at the beginning of a token (and not 's, 'm, 'll, 've, 're or 'd)
-            new Regex("(?<=^\\')(?!s$|m$|ll$|ve$|re$|d$)", RegexOptions.IgnoreCase | RegexOptions.Compiled),
-            new Regex("(?<=^’)(?!s$|m$|ll$|ve$|re$|d$)", RegexOptions.IgnoreCase | RegexOptions.Compiled),
+            "(?<=^\\')(?!s$|m$|ll$|ve$|re$|d$)",
+            "(?<=^’)(?!s$|m$|ll$|ve$|re$|d$)",
 
             // split before 's, 'm, 've, 'll, 're, 'd when at the end of a token (’ == ')
-            new Regex("(?=\\'s$|\\'m$|\\'ve$|\\'ll$|\\'re$|\\'d$|’s$|’m$|’ve$|’ll$|’re$|’d$)", RegexOptions.IgnoreCase | RegexOptions.Compiled)
+            "(?=\\'s$|\\'m$|\\'ve$|\\'ll$|\\'re$|\\'d$|’s$|’m$|’ve$|’ll$|’re$|’d$)"
         };
-
+        private static readonly Regex TokenizationRegex = new Regex(string.Format("({0})", string.Join("|", TokenizationRules)), RegexOptions.Compiled | RegexOptions.IgnoreCase);
         private static readonly Regex LettersOnlyRegex = new Regex("^[a-zA-Z]+$", RegexOptions.Compiled);
-        private List<Span> SplitToken(string input, Span span)
+        private IEnumerable<Span> SplitToken(string input, Span span)
         {
             var token = input.Substring(span.Start, span.Length());
             if (string.IsNullOrEmpty(token))
@@ -120,54 +127,16 @@ namespace OpenNLP.Tools.Tokenize
                 return new List<Span>(){ span };
             }
 
-            var splitTokens = new List<string>() { token };
-            foreach (var tokenizationRegex in TokenizationRegexes)
-            {
-                /*var tempSpans = new List<Span>();
-                foreach (var tempSpan in spans)
-                {
-                    var tempToken = input.Substring(tempSpan.Start, tempSpan.Length());
-                    var matches = tokenizationRegex.Matches(tempToken);
-                    
-                    var matchIndices = new List<int>();
-                    for (int i = 0; i < matches.Count; i++)
-                    {
-                        var index = matches[i].Index;
-                        if (0 < index && index < tempToken.Length)
-                        {
-                            matchIndices.Add(index);
-                        }
-                    }
-
-                    if (matchIndices.Any())
-                    {
-                        for (var i = 0; i < matchIndices.Count; i++)
-                        {
-                            var start = i == 0 ? 0 : matchIndices[i - 1] - 1;
-                            tempSpans.Add(new Span(tempSpan.Start + start, tempSpan.Start + matchIndices[i]));
-                        }
-                        // add last one
-                        tempSpans.Add(new Span(matchIndices.Last(), tempSpan.End));
-                    }
-                    else
-                    {
-                        tempSpans.Add(tempSpan);
-                    }
-                }
-                spans = tempSpans;*/
-
-                var tempTokens = splitTokens
-                    .SelectMany(tok => tokenizationRegex.Split(tok))
-                    .Where(p => !string.IsNullOrEmpty(p))
-                    .ToList();
-                splitTokens = tempTokens;
-            }
+            var splitTokens = TokenizationRegex.Split(token);
 
             var spans = new List<Span>();
             var currentStart = span.Start;
             foreach (var splitToken in splitTokens)
             {
-                spans.Add(new Span(currentStart, currentStart + splitToken.Length));
+                if (splitToken.Length > 0)
+                {
+                    spans.Add(new Span(currentStart, currentStart + splitToken.Length)); 
+                }
                 currentStart += splitToken.Length;
             }
             return spans;
